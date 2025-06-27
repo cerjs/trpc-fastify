724ec9328b9af1396a5322ce56648796e6473dd4
xihengfei <xihengfei@bytedance.com>
2025-06-27 14:55:22 +0800

    fix: enable tsx stderr log

--- src/server.ts
+++ src/server.ts
@@ -11,11 +11,15 @@ server.register(fastifyTRPCPlugin, {
   trpcOptions: { router: appRouter, createContext },
 });
 
-(async () => {
+
+const start = async () => {
   try {
-    await server.listen({ port: 3001 });
+    await server.listen({ port:3001 });
+    console.log('listening on port', 3001);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
-})();
+};
+
+void start();
